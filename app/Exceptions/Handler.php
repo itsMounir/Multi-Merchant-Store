27d0015de602c1f751361ff482f9d4fc2fce55a4
f ga ff <marioandrawos02@gamil.com>
2024-05-31 15:04:35 +0300

    updateeeeeeeeeeeeeeeeeeeeeee

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -69,8 +69,13 @@ class Handler extends ExceptionHandler
 
 
 
+        if ($e instanceof QueryException) {
+            return response()->json([
+                'message' => 'unknown query exception',
+            ]);
+        }
 
- 
+
 
         if ($e instanceof RouteNotFoundException) {
             return response()->json([
