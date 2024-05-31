49bfb52f024b65ce917e706a5b8b7ee91f945dd6
Haidar-Mo <165487149+Haidar-Mo@users.noreply.github.com>
2024-05-31 17:47:29 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'notification_token',
+        'deviceToken',
     ];
 
     /**
